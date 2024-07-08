932c750df3d8b71529f9a72a61e5404325293ecb
dismaster <rene.golz84@gmail.com>
2024-07-08 15:02:49 +0200

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -7,5 +7,6 @@ bat_stats=$(termux-battery-status | tr -d '\n' | tr -d ' ' | jq)
 ip_addr=$(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1)
 ccminer_summary=$(echo 'summary' | nc 127.0.0.1 4068 | tr -d '\0' | sed -r 's/=/\":\"/g; s/;/\",\"/g' | sed 's/|/\"}/g' | sed 's/^/{\"/g' | jq)
 ccminer_pool=$(echo 'pool' | nc 127.0.0.1 4068 | tr -d '\0' | sed -r 's/=/\":\"/g; s/;/\",\"/g' | sed 's/|/\"}/g' | sed 's/^/{\"/g' | jq)
+cpu_temp=$(~/vcgencmd measure_temp 2>/dev/null | tr -d '\0' | sed 's/^/{\"/g' | sed -r 's/=/\":\"/g; s/;/\",\"/g' | sed "s/'C/\"}/g" | sed 's/""/"0"/g' | jq)
 
-curl -d "hw_model=$hw_model&hw_brand=$hw_brand&ip=$ip_addr&password=$rig_pw&battery=$bat_stats&summary=$ccminer_summary&pool=$ccminer_pool" -H "Content-Type: application/x-www-form-urlencoded" -X POST https://api.rg3d.eu:8443/api.php --insecure
+curl -d "hw_model=$hw_model&hw_brand=$hw_brand&ip=$ip_addr&password=$rig_pw&battery=$bat_stats&summary=$ccminer_summary&pool=$ccminer_pool&cpu_temp=$cpu_temp" -H "Content-Type: application/x-www-form-urlencoded" -X POST https://api.rg3d.eu:8443/api.php --insecure
